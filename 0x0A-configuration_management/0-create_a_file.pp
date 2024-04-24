32062584be6af27b9a82e1de0561da826c8ff6c3
Wondwosen <revatoel@gmail.com>
2024-04-24 13:54:41 -0700

    fix puppet lint warnings

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # create a file and set permissions to it
 file {'/tmp/school' :
-  ensure => present,
-  content => 'I love Puppet'
-  mode => '0744'
-  owner => 'www-data'
-  group => 'www-data'
+  ensure  => present,
+  content => 'I love Puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
 }
 
