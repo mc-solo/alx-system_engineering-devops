9d8b6ceb2ee8e7ca90fb1938e17eefbf9cfc907f
Wondwosen <revatoel@gmail.com>
2024-04-24 14:16:48 -0700

    replace pip3 with pip

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,7 @@
 # installing flask from pip3 using puppet
 
 package {'flask':
-  ensure   => '2.1.0', #to install this specific version instead of the latest one
-  provider => 'pip3',
+  ensure   => '2.1.0', # to install this specific version instead of the latest one
+  provider => 'pip', # can't directly use pip3
 }
 
